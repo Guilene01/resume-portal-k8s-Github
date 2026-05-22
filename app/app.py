1ed5cc17969e182f24013269370769ad2ac1f649
guilene1 <guilenetiako@utrains.org>
2026-05-22 08:55:05 -0400

    trigger: deploy pipeline

--- app/app.py
+++ app/app.py
@@ -220,4 +220,4 @@ def _cors_response(body, status):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)# redeploy
